fec37d12136f4ecc30d467a6d4654bcbe0a2f7b7
simengzhu <siz122@eng.ucsd.edu>
2018-09-25 00:23:59 -0700

    Created UI components for add form

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -7,8 +7,28 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+ * GET pages index
+ */
 router.get('/', function(req, res) {
     res.send('admin area');
+});
+
+/*
+ * GET add page index
+ */
+router.get('/add-page', function(req, res) {
+    
+    var title = "";
+    var slug = "";
+    var content = "";
+    
+    res.render('admin/add_page', {
+        title: title, 
+        slug: slug,
+        content: content
+    })
+    
 });
 
 // Exports
