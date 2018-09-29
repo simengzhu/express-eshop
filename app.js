e96634759213aaacc6eaf9f155a296b305057f80
simengzhu <siz122@eng.ucsd.edu>
2018-09-29 13:25:15 -0700

    Made categories show up on client pages

--- app.js
+++ app.js
@@ -45,6 +45,18 @@ Page.find({}).sort({sorting: 1}).exec(function(err, pages) {
     }
 });
 
+// Get category model
+var Category = require('./models/category');
+
+// Get all categories to pass to header.ejs
+Category.find(function(err, categories) {
+    if (err) {
+        console.log(err);
+    } else {
+        app.locals.categories = categories;
+    }
+});
+
 // Express fileUpload middleware
 app.use(fileUpload());
 
--- routes/admin_categories.js
+++ routes/admin_categories.js
@@ -73,6 +73,14 @@ router.post('/add-category', [
                     if (err) {
                         return console.log(err);
                     }
+                    
+                    Category.find(function(err, categories) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            req.app.locals.categories = categories;
+                        }
+                    });
 
                     req.flash('success', 'Category added');
                     res.redirect('/admin/categories');
@@ -137,6 +145,14 @@ router.post('/edit-category/:id', [
                         if (err) {
                             return console.log(err);
                         }
+                        
+                        Category.find(function(err, categories) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                req.app.locals.categories = categories;
+                            }
+                        });
 
                         req.flash('success', 'Category edited');
                         res.redirect('/admin/categories/edit-category/'+id);
@@ -155,6 +171,14 @@ router.get('/delete-category/:id', function(req, res) {
         if (err) {
             return console.log(err);
         } 
+        
+        Category.find(function(err, categories) {
+            if (err) {
+                console.log(err);
+            } else {
+                req.app.locals.categories = categories;
+            }
+        });
         
         req.flash('success', 'Category deleted');
         res.redirect('/admin/categories/');
