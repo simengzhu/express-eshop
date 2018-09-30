85fef0ff4a985c626cdb0e0b61fec104f72b4b67
simengzhu <siz122@eng.ucsd.edu>
2018-09-30 14:49:03 -0700

    Added logout functionality and bug fixes

--- routes/users.js
+++ routes/users.js
@@ -111,5 +111,17 @@ router.post('/login', function (req, res, next) {
     
 });
 
+/*
+ * GET logout
+ */
+router.get('/logout', function (req, res) {
+
+    req.logout();
+    
+    req.flash('success', 'You are logged out');
+    res.redirect('/users/login');
+
+});
+
 // Exports
 module.exports = router;
